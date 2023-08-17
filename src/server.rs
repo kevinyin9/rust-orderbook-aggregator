f35d1d81f414d049809c5a22787026a744b67542
Huan-Wen (Kevin) Yin <kevinyin999@gmail.com>
2023-08-17 17:06:44 +0800

    Modify gRPC stream

--- src/server.rs
+++ src/server.rs
@@ -1,21 +1,49 @@
 use config::Config;
 use std::collections::HashMap;
-use tonic::{transport::Server, Status};
-use orderbook::{OrderbookAggregator, Summary, Level}
+use tokio::{sync::mpsc, sync::oneshot, sync::watch};
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod orderbook {
+    tonic::include_proto!("orderbook");
+}
+
+use orderbook::{
+    orderbook_aggregator_server::{OrderbookAggregator, OrderbookAggregatorServer},
+    Summary, Level,
+};
+
+#[derive(Default)]
+pub struct OrderbookSummary {}
 
 #[tonic::async_trait]
-impl OrderbookAggregator for MyOrderbook {
-    async fn BookSummary(
-        &self
-    ) -> Result<()> { // Return an instance of type HelloReply
+impl OrderbookAggregator for OrderbookSummary {
+    type SummaryStream = Pin<Box<dyn Stream<Item = Result<Summary, Status>> + Send>>;
+    async fn book_summary(
+        &self,
+        request: Request<Summary>,
+    ) -> Result<tonic::Response<Self::SummaryStream>, Status> { // Return an instance of type HelloReply
         println!("Got a request");
         
-        Ok() // Send back our formatted greeting
+        let reply = orderbook::Summary{
+            spread: 6.4,
+            bids: vec![orderbook::Level{exchange: "binance".to_string(),
+                price: 3.5,
+                amount: 1.0,
+            }],
+            asks: vec![orderbook::Level{exchange: "binance".to_string(),
+                price: 3.5,
+                amount: 1.0,
+            }],
+        };
+
+        Ok(tonic::Response::new(
+            Box::pin(stream) as Self::SummaryStream
+        ))
     }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), E> { // Should i return Result<(), Error>?
+async fn main() -> Result<(), Box<dyn std::error::Error>> { // Should i return Result<(), Error>?
     let config = Config::builder()
         // read the setting.toml
         .add_source(config::File::with_name("src/setting"))
@@ -24,11 +52,12 @@ async fn main() -> Result<(), E> { // Should i return Result<(), Error>?
         .try_deserialize::<HashMap<String, String>>()
         .unwrap();
 
-    let orderbook = MyOrderbook::default();
+    let orderbook = OrderbookSummary::deafult();
 
     Server::builder()
-    .add_service(GreeterServer::new(orderbook))
+    .add_service(OrderbookAggregatorServer::new(orderbook))
     .serve(config["server-ip"].parse()?)
     .await?;
+
     Ok(())
 }
